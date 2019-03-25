bdc640364b01d221e38a5bc2a14a971cf1199e3b
Meg Galvez <meg@oakmaine.com>
2019-03-25 23:39:51 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,10 +1,18 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  dwarves = (names)
+ 
+  dwarves.each.with_index(1) do |value, index|
+    puts "#{index}: #{value}"
+end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
+def summon_captain_planet(calls)
+  calls_array = (calls)
+  calls_array.collect do |w| 
+    w << "!"
+    w.capitalize
+end 
+end 
 
 def long_planeteer_calls# code an argument here
   # Your code here
