2f7f3928404f47e613baa36aba55e5cf3c2d2e6a
Meg Galvez <meg@oakmaine.com>
2019-03-26 13:49:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,7 +24,12 @@ def long_planeteer_calls(calls)
   end 
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  if cheese.include(cheese_types)
+    
+  
+  
+  
 end
