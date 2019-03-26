e7ca627b453d0fe0c9230876219bd9d55d9a7bc0
Meg Galvez <meg@oakmaine.com>
2019-03-26 13:24:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,8 +14,7 @@ def summon_captain_planet(calls)
 end 
 end 
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls
 end
 
 def find_the_cheese# code an argument here
