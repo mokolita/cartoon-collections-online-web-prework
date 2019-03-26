b655522103d56b2e7a80aa1cc8c57db90ff1a8b9
Meg Galvez <meg@oakmaine.com>
2019-03-26 13:52:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -36,3 +36,22 @@ def find_the_cheese(cheese)
   
   
 end
+
+
+
+
+def long_planeteer_calls(calls)
+  calls.any? do |call|
+    call.length > 4
+  end
+end
+
+def find_the_cheese(array)
+  # the array below is here to help
+  i = 0
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  while i < cheese_types.length
+    return cheese_types[i] if array.include?(cheese_types[i])
+    i += 1
+  end
+end
