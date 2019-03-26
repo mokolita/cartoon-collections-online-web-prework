7279c964630fbeb8052e8893b96acd26407f71f1
Meg Galvez <meg@oakmaine.com>
2019-03-26 13:57:35 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,11 +14,25 @@ def summon_captain_planet(calls)
 end 
 end 
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? do |call|
+    call.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  cheese.find do |item|
+    cheese_types.include?(item)
+  end
 end
+  
+
+
+
+
+
+
+
+
